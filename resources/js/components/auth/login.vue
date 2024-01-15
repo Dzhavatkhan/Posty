05114e4509ba6ff4604fdf0a40817920c23284d9
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-01-15 17:28:29 +0400

    super pro

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="conatainer w-[1903px] bg-gray-100 h-screen flex justify-center items-center">
-        <div class="form bg-white shadow-md w-[450px] h-[750px] flex flex-col justify-center items-center gap-[150px]">
+        <div class="form bg-white shadow-md w-[450px] h-[550px] flex flex-col justify-center items-center gap-[100px]">
                 <div class="form_title flex justify-center">
-                    <p class="p-5 text-[36px]" >Авторизация</p>
+                    <p class=" text-[36px]" >Авторизация</p>
                 </div>
                 <div class="form_body flex flex-col items-center gap-y-[35px]">
-                    <input type="text" class=" pl-3 text-xl border-gray-400 focus:border-black h-[35px] w-1/2 border outline-none" placeholder="Логин">
-                    <input type="text" class="outline-none  pl-3 text-xl border-gray-400 focus:border-black h-[35px] w-1/2 border" placeholder="Пароль">
+                    <input type="text" class=" rounded-md pl-3 text-xl border-gray-400 focus:border-black h-[35px] w-[215px] border outline-none" placeholder="Логин">
+                    <input type="text" class=" rounded-md outline-none w-[215px] pl-3 text-xl border-gray-400 focus:border-black h-[35px] border" placeholder="Пароль">
                 </div>
                 <div class="form_footer">
-                    <button class="text-xl">Войти</button>
+                    <button class="text-xl hover:border-black border duration-[0.5s] hover:text-black hover:bg-white bg-black  text-white px-[100px] rounded-md py-3">Войти</button>
+                    <a href="" class="block text-blue-400">У вас нет аккаунта?</a>
                 </div>
 
         </div>
