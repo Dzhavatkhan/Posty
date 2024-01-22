e812a11472a40323349b90ce6edfea1c94e313f0
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-01-22 15:32:45 +0400

    dsgffigoidhfsgi

--- resources/js/components/auth/profile.vue
+++ resources/js/components/auth/profile.vue
@@ -22,7 +22,7 @@
             <div class="my_posts w-full flex flex-wrap justify-center gap-10 py-10">
                 <div v-for="post in posts" :key="post.id"   class="flex px-[15px]  py-5 justify-between bg-white shadow-md max-[320px]:w-full max-[600px]:w-full max-[680px]:w-full w-80 flex-col max-[430px]:w-full">
                     <div class="mb-5 flex justify-start flex-col gap-y-[25px] max-[430px]:text-sm">
-                        <div class="post_title text-xl flex justify-start items-center border-[#A8A8A8] border-b">
+                        <div @click="open = !open" class="post_title text-xl flex justify-start items-center border-[#A8A8A8] border-b">
                             {{ post.title }}
                         </div>
 
@@ -42,10 +42,59 @@
                         </div>
 
                     </div>
+
+
+                    <div v-if="open" id="static-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative p-4 w-full max-w-2xl max-h-full">
+                            <!-- Modal content -->
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <!-- Modal header -->
+                                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                        {{post.title}}`
+                                    </h3>
+                                    <button type="button" @click="open = !open" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <div class="p-4 md:p-5 space-y-4">
+                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        {{ post.body }}
+                                    </p>
+                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                                    </p>
+                                </div>
+                                <!-- Modal footer -->
+                                <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                    <button data-modal-hide="static-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                                    <button data-modal-hide="static-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
+
+
+
+
+
+
+
+
+
+
+
             </div>
         </div>
     </div>
+
 
     <!-- <div id="modalShow" class="modalShow relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
           Background backdrop, show/hide based on modal state.
@@ -100,6 +149,8 @@ import {onMounted, ref} from 'vue'
 import { useRouter } from 'vue-router';
 
 let router = useRouter();
+
+let open = ref(false)
 
 let posts = ref([])
 let src = ref('http://127.0.0.1:8000/image/free-icon-heart-shape-outline-with-lining-at-right-edge-31611.png')
